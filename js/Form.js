25df8d58a21001507662936e646f64e35ca5a4b7
procodingclass@whitehatjr.com <procodingclass@whitehatjr.com>
2021-08-26 12:59:05 +0530

    Input Box Position Changed

--- js/Form.js
+++ js/Form.js
@@ -8,9 +8,9 @@ class Form {
 
   setElementsPosition() {
     this.titleImg.position(120, 50);
-    this.input.position(width / 2 - 110, height / 2);
-    this.playButton.position(width / 2 - 90, height / 2 + 80);
-    this.greeting.position(width / 2 - 300, height / 2 - 100);
+    this.input.position(width / 2 - 110, height / 2 + 20);
+    this.playButton.position(width / 2 - 90, height / 2 + 100);
+    this.greeting.position(width / 2 - 300, height / 2);
   }
 
   setElementsStyle() {
